72ed24f857ea8f81bbe08504fab91e747ff78a5b
pepo023 <pepo023@outlook.com>
2018-11-24 21:06:01 -0600

    python coding brain added

--- brain.py
+++ brain.py
@@ -8,7 +8,7 @@ def brain(name, speech_text):
     def check_message(check):
         """"
         This function checks if the items in the list (specified in argument) are
-        present in the user's input speech.
+        present in the user's input speech. Is "check" a subset of the user's input?
         """
         words_of_message = speech_text.split()  # separate input string into list of strings
         if set(check).issubset(set(words_of_message)):  # set removes any duplicates from word list
